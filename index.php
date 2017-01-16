d25f2d5b866217ab8b8e92add84dcf4a04df9a5d
SceneTeam <omidbakhsh22@gmail.com>
2017-01-16 20:47:55 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('API_KEY','توکن ربات');
+define('API_KEY','توکن شما');
 //----######------
 function makereq($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
@@ -53,9 +53,7 @@ $textmessage = isset($update->message->text)?$update->message->text:'';
 $txtmsg = $update->message->text;
 $reply = $update->message->reply_to_message->forward_from->id;
 $stickerid = $update->message->reply_to_message->sticker->file_id;
-$admin = ایدی عدد شما;
-$userbot = "یوزرنیم ربا شما همرا با @";
-$channel = "یوزرنیم کانال همرا با @";
+$admin = ایدی عددی شما;
 $step = file_get_contents("data/".$from_id."/step.txt");
 $ban = file_get_contents('data/banlist.txt');
 //-------
@@ -89,11 +87,11 @@ function save($filename,$TXTdata)
 	fclose($myfile);
 	}
 //===========
-$inch = file_get_contents("https://api.telegram.org/bot".API_KEY."/getChatMember?chat_id=@"$channel"&user_id=".$from_id);
+$inch = file_get_contents("https://api.telegram.org/bot".API_KEY."/getChatMember?chat_id=@Channel&user_id=".$from_id);
 	
 	if (strpos($inch , '"status":"left"') !== false ) {
 SendMessage($chat_id,"برای استفاده از ربات اول در کانال ما عضو شوید.
-@"$channel"");
+@Channel");
 }
 if (strpos($ban , "$from_id") !== false  ) {
 SendMessage($chat_id,"You Are Banned From Server.🤓\nDon't Message Again...😎\n➖➖➖➖➖➖➖➖➖➖\nدسترسی شما به این سرور مسدود شده است.🤓\nلطفا پیام ندهید...😎");
@@ -122,7 +120,7 @@ elseif(isset($update->callback_query)){
             'reply_markup'=>json_encode([
                 'inline_keyboard'=>[
                     [
-                        ['text'=>"به کانال ما بپیوندید - Pease join to my channel",'url'=>"https://telegram.me/"$channel""]
+                        ['text'=>"به کانال ما بپیوندید - Pease join to my channel",'url'=>"https://telegram.me/"]
                     ]
                 ]
             ])
@@ -138,7 +136,7 @@ elseif(isset($update->callback_query)){
             'reply_markup'=>json_encode([
                 'inline_keyboard'=>[
                     [
-                        ['text'=>"به کانال ما بپیوندید - Pleas join to my channel",'url'=>"https://telegram.me/"$channel""]
+                        ['text'=>"به کانال ما بپیوندید - Pleas join to my channel",'url'=>"https://telegram.me/"]
                     ]
                 ]
             ])
@@ -236,7 +234,7 @@ $token = $textmessage ;
 		$source = str_replace("**TOKEN**",$token,$source);
 		$source = str_replace("**ADMIN**",$from_id,$source);
 		save("bots/$un/index.php",$source);	
-		file_get_contents("http://api.pwrtelegram.xyz/bot".$token."/setwebhook?url=ادرس webhook/bots/$un/index.php");
+		file_get_contents("http://api.pwrtelegram.xyz/bot".$token."/setwebhook?url=https://testpv-tgmassenger.rhcloud.com/bots/$un/index.php");
 
 var_dump(makereq('sendMessage',[
         	'chat_id'=>$update->message->chat->id,
@@ -300,7 +298,7 @@ Your Robot Has ben Created
 		$source = str_replace("[*BOTTOKEN*]",$token,$source);
 		$source = str_replace("66443035",$from_id,$source);
 		save("bots/$un/index.php",$source);	
-		file_get_contents("http://api.pwrtelegram.xyz/bot".$token."/setwebhook?url=ادرس webhook/bots/$un/index.php");
+		file_get_contents("http://api.pwrtelegram.xyz/bot".$token."/setwebhook?url=https://testpv-tgmassenger.rhcloud.com/bots/$un/index.php");
 
 var_dump(makereq('sendMessage',[
         	'chat_id'=>$update->message->chat->id,
@@ -361,7 +359,7 @@ else {
 SendMessage($chat_id,"You Are banned");
 }
 }
-elseif($textmessage == '🚀 ربات های من' || '🚀 my robots')
+elseif($textmessage == '🚀 ربات های من')
 {
 $botname = file_get_contents("data/$from_id/bots.txt");
 if ($botname == "") {
@@ -381,7 +379,83 @@ return;
 	])
 	]));
 }
-elseif($textmessage == '🇦🇺 تغییر زبان 🇮🇷' || '🇦🇺 Language 🇮🇷' || '/start' )
+elseif($textmessage == '🚀 my robots')
+{
+$botname = file_get_contents("data/$from_id/bots.txt");
+if ($botname == "") {
+SendMessage($chat_id,"You still have not robots!");
+return;
+}
+ 	var_dump(makereq('sendMessage',[
+	'chat_id'=>$update->message->chat->id,
+	'text'=>"Your Bot Lists : ",
+	'parse_mode'=>'MarkDown',
+	'reply_markup'=>json_encode([
+	'inline_keyboard'=>[
+	[
+	['text'=>"👉 @".$botname,'url'=>"https://telegram.me/".$botname]
+	]
+	]
+	])
+	]));
+}
+elseif($textmessage == '/start' )
+{
+if (!file_exists("data/$from_id/step.txt")) {
+mkdir("data/$from_id");
+save("data/$from_id/step.txt","none");
+save("data/$from_id/tedad.txt","0");
+save("data/$from_id/bots.txt","");
+$myfile2 = fopen("data/users.txt", "a") or die("Unable to open file!"); 
+fwrite($myfile2, "$from_id\n");
+fclose($myfile2);
+}
+
+var_dump(makereq('sendMessage',[
+         'chat_id'=>$update->message->chat->id,
+         'text'=>"Please Select your Language.
+➖➖➖➖➖
+لطفا زبان حود را انتخاب کنید.",
+  'parse_mode'=>'MarkDown',
+         'reply_markup'=>json_encode([
+             'keyboard'=>[
+[
+                   ['text'=>"فارسی 🇮🇷"],['text'=>"English 🇦🇺"]          
+]
+             ],
+             'resize_keyboard'=>true
+         ])
+      ]));
+}
+elseif($textmessage == '🇦🇺 تغییر زبان 🇮🇷' )
+{
+if (!file_exists("data/$from_id/step.txt")) {
+mkdir("data/$from_id");
+save("data/$from_id/step.txt","none");
+save("data/$from_id/tedad.txt","0");
+save("data/$from_id/bots.txt","");
+$myfile2 = fopen("data/users.txt", "a") or die("Unable to open file!"); 
+fwrite($myfile2, "$from_id\n");
+fclose($myfile2);
+}
+
+var_dump(makereq('sendMessage',[
+         'chat_id'=>$update->message->chat->id,
+         'text'=>"Please Select your Language.
+➖➖➖➖➖
+لطفا زبان حود را انتخاب کنید.",
+  'parse_mode'=>'MarkDown',
+         'reply_markup'=>json_encode([
+             'keyboard'=>[
+[
+                   ['text'=>"فارسی 🇮🇷"],['text'=>"English 🇦🇺"]          
+]
+             ],
+             'resize_keyboard'=>true
+         ])
+      ]));
+}
+elseif($textmessage == '🇦🇺 Language 🇮🇷' )
 {
 if (!file_exists("data/$from_id/step.txt")) {
 mkdir("data/$from_id");
@@ -430,9 +504,7 @@ var_dump(makereq('sendMessage',[
 
 🔸 with this Service you can Provide Support  your Robot Mmbers , Channel , Groups and  Websites 
 
-🔹 To Create Robot Select '🔄 Create a Robot' Button!
-
-🤖 @"$userbot"",
+🔹 To Create Robot Select '🔄 Create a Robot' Button!",
   'parse_mode'=>'MarkDown',
          'reply_markup'=>json_encode([
              'keyboard'=>[
@@ -474,9 +546,7 @@ var_dump(makereq('sendMessage',[
 
 🔸 با استفاده از این سرویس شما میتوانید رباتی جهت ارائه پشتیبانی به کاربران ربات، کانال، گروه یا وبسایت خود ایجاد کنید.
 
-🔹برای ساخت ربات از دکمه ی 🔄 ساخت ربات استفاده نمایید.
-
-🤖 @"$userbot"",
+🔹برای ساخت ربات از دکمه ی 🔄 ساخت ربات استفاده نمایید.",
   'parse_mode'=>'MarkDown',
          'reply_markup'=>json_encode([
              'keyboard'=>[
@@ -498,25 +568,37 @@ var_dump(makereq('sendMessage',[
          ])
       ]));
 }
-elseif($textmessage == '🔰 قوانین' || '🔰 rules') {
-SendMessage($chat_id, "1⃣ اطلاعات ثبت شده در ربات های ساخته شده توسط پیوی رسان از قبیل اطلاعات پروفایل نزد مدیران پیوی رسان محفوظ است و در اختیار اشخاص حقیقی یا حقوقی قرار نخواهد گرفت.\n\n2⃣ ربات هایی که اقدام به انشار تصاویر یا مطالب مستهجن کنند و یا به مقامات ایران ، ادیان و اقوام و نژادها توهین کنند مسدود خواهند شد.\n\n3⃣ ایجاد ربات با عنوان های مبتذل و خارج از عرف جامعه که برای جذب آمار و فروش محصولات غیر متعارف باشند ممنوع می باشد و در صورت مشاهده ربات مورد نظر حذف و مسدود میشود.\n\n4⃣ مسئولیت پیام های رد و بدل شده در هر ربات با مدیر آن ربات میباشد و پیوی رسان هیچ گونه مسئولیتی قبول نمیکند.\n\n5⃣ رعایت حریم خصوصی و حقوقی افراد از جمله، عدم اهانت به شخصیت های مذهبی، سیاسی، حقیقی و حقوقی کشور و به ویژه کاربران ربات ضروری می باشد.\n\n6⃣ ساخت هرگونه ربات و فعالیت در ضمینه های Hacking و Sexology خلاف قوانین است در صورت برخورد دستری مدیر ربات و ربات ساخته شده به سرور ما مسدود خواهد شد.\n\n7⃣ در صورت مشاهده استفاده از قابلیت های ربات در جهات منفی مانند Spam یا Hack کاربران تلگرام شدیدا بخورد میشود و تمامی اطلاعات شخص مورد نظر گزارش میشود.\n\n8⃣ اگر تعداد درخواست های ربات شما به سرور ما بیش از حد معمول باید و همچنین ربات شما VIP نباشد چند بار به شما اخطاری جهت VIP کردن ربات نمایش داده میشود اگر این اخطار نادیده گرفته شود، ربات شما به صورت اتوماتیک به حالت تعلیغ در می آید.\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\n\n1⃣ Recorded data in robots made by PvResan such as profile data , are preserved to PvResan's managers and will not be available for real or juridical people.\n\n 2⃣ Robots that publish obscene pictures or subjects and insult the officials , religions and nations and races , will be blocked.\n\n3⃣ Creating a robot with vulgar titles and out of norm of society which absorbs the statistics and selling offbeat products are prohibited and in case of witnessing  intended robot will be deleted and blocked.\n\n4⃣ The responsibility of exchanged messages in each robot is with the manager of that robot and PvResan does not accept any responsibilities.\n\n5⃣ Respecting the privacy and rights of individuals is necessary. Including no offense to religious , political and juridical figures of the country specially robot users.");
-
-elseif($textmessage == 'ℹ️ راهنما' || 'ℹ️ help') {
-SendMessage($chat_id, "What PvResan do?🤔\n\n🔶 With this Service you can Provide Support  your Robot Mmbers , Channel , Groups and  Websites\n\nSome of this Service Features :\n\n🚀 Fast Server\n\n1⃣ Send Mesage To Members Or Groups Or Both\n2⃣ Lock Sending Photos , Videos , Stickers , Documents , Voices and Audios Separately\n3⃣ Lock Forward To your Robot\n4⃣ Lock Adding Your Robots To Groups\n5⃣ Check Robot Membes And Groups\n6⃣ Check Your Black List\n7⃣ Put Members To Black List\n8⃣ Fast Share Your Contact\n9⃣ Change Your Start Text\nAnd several other features ...\n\n🔴 For information about how to get a token from @botFather use
-ربات پیام رسان چیست؟ 🤔\n\n🔶 ربات پیام رسان این امکان را به شما میدهد تا با مخاطب هایتان که ریپورت شده ایند یا به هر دلایلی نمیتواننید به شما به صورت خصوصی پیام دهند صحبت کنید.\n\nبرخی از ویژگی های ربات پیام رسان :\n\n🚀سرعت بـــــالــا\n1⃣ ارسال مطلب به تمام کاربران یا تمام گروه ها\n2⃣ قفل کردن ارسال عکس ، فیلم ، استیکر ، فایل ، وویس ، آهنگ به صورت مجزا\n3⃣ قفل کردن فروارد در ربات\n4⃣ قفل کردن عضویت ربات در گروه ها\n5⃣مشاهده تعداد اعضا و گروه ها و یوزرنیم 10 کاربر تازه عضو شده\n6⃣مشاهده تعداد اعضا در لیست سیاه و یوزرنیم 10 کاربر جدید در لیست سیاه\n7⃣ قرار دادن افراد در لیست سیاه و بن کردن آنها\n8⃣ قابلیت شیر کردن شماره ی شما به صورت سریع\n9⃣تغییر متن استارت و پیام پیشفرض\nو چندین ویژگی دیگر...");
-	
-elseif($textmessage == '✅ ارسال نظر' || '✅ Send Comment') {
-SendMessage($chat_id, "🖊 Your Comments Help Us To Be Better.\n\n📝 To Send Comment Use this Command Below.\n\n/feedback (Your Message)\n✍ نظرات شما به پیشرفت و ارائه خدمات بهتر کمک میکند.\n\n📝 برای ارسال نظر خوت از دستور زیر استفاده نمایید.\n\n/feedback (پیام خود را بنویسید)");
-elseif($textmessage == '⚠️ گزارش تخلف' || '⚠️ Robot Report') {
-SendMessage($chat_id, "اگر رباتی بر خلاف قوانین ما عمل میکند با دستور زیر به ما خبر دهید\n/report (usernamebot)\n\nIf the robot to act contrary to our laws let us know using the following command\n/report (usernamebot)");
-
-elseif ($textmessage == '☢ حذف ربات' || '☢ Delete a Robot' ) {
+elseif($textmessage == '🔰 قوانین') {
+SendMessage($chat_id, "1⃣ اطلاعات ثبت شده در ربات های ساخته شده توسط پیوی رسان از قبیل اطلاعات پروفایل نزد مدیران پیوی رسان محفوظ است و در اختیار اشخاص حقیقی یا حقوقی قرار نخواهد گرفت.\n\n2⃣ ربات هایی که اقدام به انشار تصاویر یا مطالب مستهجن کنند و یا به مقامات ایران ، ادیان و اقوام و نژادها توهین کنند مسدود خواهند شد.\n\n3⃣ ایجاد ربات با عنوان های مبتذل و خارج از عرف جامعه که برای جذب آمار و فروش محصولات غیر متعارف باشند ممنوع می باشد و در صورت مشاهده ربات مورد نظر حذف و مسدود میشود.\n\n4⃣ مسئولیت پیام های رد و بدل شده در هر ربات با مدیر آن ربات میباشد و پیوی رسان هیچ گونه مسئولیتی قبول نمیکند.\n\n5⃣ رعایت حریم خصوصی و حقوقی افراد از جمله، عدم اهانت به شخصیت های مذهبی، سیاسی، حقیقی و حقوقی کشور و به ویژه کاربران ربات ضروری می باشد.\n\n6⃣ ساخت هرگونه ربات و فعالیت در ضمینه های Hacking و Sexology خلاف قوانین است در صورت برخورد دستری مدیر ربات و ربات ساخته شده به سرور ما مسدود خواهد شد.\n\n7⃣ در صورت مشاهده استفاده از قابلیت های ربات در جهات منفی مانند Spam یا Hack کاربران تلگرام شدیدا بخورد میشود و تمامی اطلاعات شخص مورد نظر گزارش میشود.\n\n8⃣ اگر تعداد درخواست های ربات شما به سرور ما بیش از حد معمول باید و همچنین ربات شما VIP نباشد چند بار به شما اخطاری جهت VIP کردن ربات نمایش داده میشود اگر این اخطار نادیده گرفته شود، ربات شما به صورت اتوماتیک به حالت تعلیغ در.");
+}
+elseif($textmessage == '🔰 rules') {
+SendMessage($chat_id, "1⃣ Recorded data in robots made by PvResan such as profile data , are preserved to PvResan's managers and will not be available for real or juridical people.\n\n 2⃣ Robots that publish obscene pictures or subjects and insult the officials , religions and nations and races , will be blocked.\n\n3⃣ Creating a robot with vulgar titles and out of norm of society which absorbs the statistics and selling offbeat products are prohibited and in case of witnessing  intended robot will be deleted and blocked.\n\n4⃣ The responsibility of exchanged messages in each robot is with the manager of that robot and PvResan does not accept any responsibilities.\n\n5⃣ Respecting the privacy and rights of individuals is necessary. Including no offense to religious , political and juridical figures of the country specially robot users.");
+}
+elseif($textmessage == 'ℹ️ راهنما') {
+SendMessage($chat_id, "ربات پیام رسان چیست؟ 🤔\n\n🔶 ربات پیام رسان این امکان را به شما میدهد تا با مخاطب هایتان که ریپورت شده ایند یا به هر دلایلی نمیتواننید به شما به صورت خصوصی پیام دهند صحبت کنید.\n\nبرخی از ویژگی های ربات پیام رسان :\n\n🚀سرعت بـــــالــا\n1⃣ ارسال مطلب به تمام کاربران یا تمام گروه ها\n2⃣ قفل کردن ارسال عکس ، فیلم ، استیکر ، فایل ، وویس ، آهنگ به صورت مجزا\n3⃣ قفل کردن فروارد در ربات\n4⃣ قفل کردن عضویت ربات در گروه ها\n5⃣مشاهده تعداد اعضا و گروه ها و یوزرنیم 10 کاربر تازه عضو شده\n6⃣مشاهده تعداد اعضا در لیست سیاه و یوزرنیم 10 کاربر جدید در لیست سیاه\n7⃣ قرار دادن افراد در لیست سیاه و بن کردن آنها\n8⃣ قابلیت شیر کردن شماره ی شما به صورت سریع\n9⃣تغییر متن استارت و پیام پیشفرض\nو چندین ویژگی دیگر...");
+}
+elseif($textmessage == 'ℹ️ help') {
+SendMessage($chat_id, "What PvResan do?🤔\n\n🔶 With this Service you can Provide Support  your Robot Mmbers , Channel , Groups and  Websites\n\nSome of this Service Features :\n\n🚀 Fast Server\n\n1⃣ Send Mesage To Members Or Groups Or Both\n2⃣ Lock Sending Photos , Videos , Stickers , Documents , Voices and Audios Separately\n3⃣ Lock Forward To your Robot\n4⃣ Lock Adding Your Robots To Groups\n5⃣ Check Robot Membes And Groups\n6⃣ Check Your Black List\n7⃣ Put Members To Black List\n8⃣ Fast Share Your Contact\n9⃣ Change Your Start Text\nAnd several other features ...\n\n🔴 For information about how to get a token from @botFather use");
+}
+elseif($textmessage == '✅ ارسال نظر' ) {
+SendMessage($chat_id, "🖊 نظرات شما به پیشرفت و ارائه خدمات بهتر کمک میکند.\n\n📝 برای ارسال نظر خوت از دستور زیر استفاده نمایید.\n\n/feedback (پیام خود را بنویسید)");
+}
+	elseif($textmessage == '⚠️ گزارش تخلف' ) {
+SendMessage($chat_id, "اگر رباتی بر خلاف قوانین ما عمل میکند با دستور زیر به ما خبر دهید\n/report (usernamebot)");
+}
+elseif($textmessage == '✅ Send Comment') {
+SendMessage($chat_id, "🖊 Your Comments Help Us To Be Better.\n\n📝 To Send Comment Use this Command Below.\n\n/feedback (Your Message)");
+}
+	elseif($textmessage ==  '⚠️ Robot Report') {
+SendMessage($chat_id, "If the robot to act contrary to our laws let us know using the following command\n/report (usernamebot)");
+}
+elseif ($textmessage == '☢ حذف ربات' ) {
 if (file_exists("data/$from_id/step.txt")) {
 
 }
 $botname = file_get_contents("data/$from_id/bots.txt");
 if ($botname == "") {
-SendMessage($chat_id,"شما هنوز هیچ رباتی نساخته اید !\nDo robots have not");
+SendMessage($chat_id,"شما هنوز هیچ رباتی نساخته اید !");
 
 }
 else {
@@ -525,7 +607,7 @@ else {
 
  	var_dump(makereq('sendMessage',[
 	'chat_id'=>$update->message->chat->id,
-	'text'=>"یکی از ربات های خود را انتخاب کنید : \nSelect one of your robots:",
+	'text'=>"یکی از ربات های خود را انتخاب کنید :",
 	'parse_mode'=>'MarkDown',
 	'reply_markup'=>json_encode([
 	'inline_keyboard'=>[
@@ -556,58 +638,24 @@ var_dump(makereq('sendMessage',[
     		])); */
 }
 }
-elseif ($textmessage == '🔄 ساخت ربات' || '🔄 Create a Robot') {
+elseif ($textmessage == '☢ Delete a Robot' ) {
+if (file_exists("data/$from_id/step.txt")) {
 
-$tedad = file_get_contents("data/$from_id/tedad.txt");
-if ($tedad >= 1) {
-SendMessage($chat_id,"تعداد ربات های ساخته شده شما زیاد است !
-اول باید یک ربات را پاک کنید ! $tedad
-Each person can only build other robots you can build a robot");
-return;
 }
-save("data/$from_id/step.txt","create bot");
-var_dump(makereq('sendMessage',[
-        	'chat_id'=>$update->message->chat->id,
-        	'text'=>"توکن را وارد کنید :\n Please send your token ",
-		'parse_mode'=>'MarkDown',
-        	'reply_markup'=>json_encode([
-            	'keyboard'=>[
-                [
-                   ['text'=>"🔙 برگشت"]
-                ]
-                
-            	],
-            	'resize_keyboard'=>true
-       		])
-    		]));
-}
-	elseif (strpos($textmessage , "/ban" ) !== false ) {
-if ($from_id == $admin) {
-$text = str_replace("/ban","",$textmessage);
-$myfile2 = fopen("data/banlist.txt", 'a') or die("Unable to open file!");	
-fwrite($myfile2, "$text\n");
-fclose($myfile2);
-SendMessage($admin,"شما کاربر $text را در لیست بن لیست قرار دادید!\n برای در اوردن این کاربر از بن از دستور زیر استفاده کنید\n/unban $text");
+$botname = file_get_contents("data/$from_id/bots.txt");
+if ($botname == "") {
+SendMessage($chat_id,"Do robots have not");
+
 }
 else {
-SendMessage($chat_id,"⛔️ شما ادمین نیستید.");
-}
-}
+//save("data/$from_id/step.txt","delete");
 
-elseif (strpos($textmessage , "/unban" ) !== false ) {
-if ($from_id == $admin) {
-$text = str_replace("/unban","",$textmessage);
-			$newlist = str_replace($text,"",$ban);
-			save("data/banlist.txt",$newlist);
-SendMessage($admin,"شما کاربر $text را از لیست بن ها در اوردید!");
-}
-else {
-SendMessage($chat_id,"⛔️ شما ادمین نیستید.");
-}
-}
-	
-else
-{
-SendMessage($chat_id,"پیام شما پیدا نشد❌\n Your command could not be found❌");
-}
-?>
+
+ 	var_dump(makereq('sendMessage',[
+	'chat_id'=>$update->message->chat->id,
+	'text'=>"Select one of your robots:",
+	'parse_mode'=>'MarkDown',
+	'reply_markup'=>json_encode([
+	'inline_keyboard'=>[
+	[
+	['text'=>"👉 @".$botname,'callback_data'=>"del ".$botname]
